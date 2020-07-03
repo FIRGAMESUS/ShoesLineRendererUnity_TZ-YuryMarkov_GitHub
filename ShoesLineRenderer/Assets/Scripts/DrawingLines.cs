93b3c05db19415d1e83c5ad600710080f1d32927
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-03 17:52:05 +0300

    Drawing All Coords

--- ShoesLineRenderer/Assets/Scripts/DrawingLines.cs
+++ ShoesLineRenderer/Assets/Scripts/DrawingLines.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DrawingLines : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject model;
+    [SerializeField]
+    private GameObject lineGeneratorPrefab;
+    public List<Vector3> coords;
+
     void Start()
     {
-        
+        model = GameObject.Find("Result");
+        lineGeneratorPrefab = GameObject.Find("LineHolder");
+        EnumerationCoordsPolygon();
+        WriteMeshPoints();
     }
 
     
@@ -18,7 +26,7 @@ public class DrawingLines : MonoBehaviour
 
     void EnumerationCoordsPolygon()
     {
-        Mesh mesh = model.GetComponent<MeshFilter>().mesh;
+        Mesh mesh = model.transform.GetChild(0).GetComponent<MeshFilter>().mesh;
         Vector3[] verts = mesh.vertices;
         int[] triangles = mesh.triangles;
         for (int i = 0; i < triangles.Length; i += 3)
@@ -30,7 +38,22 @@ public class DrawingLines : MonoBehaviour
             Vector3 v1 = verts[triangles[i1]];
             Vector3 v2 = verts[triangles[i2]];
             Vector3 v3 = verts[triangles[i3]];
+            Debug.Log(v1 + " " + v2 + " " + v3);
+            coords.Add(v1);
+            //coords.Add(v2);
+            //coords.Add(v3);
         }
 
+    }
+
+    void WriteMeshPoints()
+    {
+        GameObject newLineGen = Instantiate(lineGeneratorPrefab);
+        LineRenderer lRend = newLineGen.GetComponent<LineRenderer>();
+        lRend.positionCount = coords.Count;
+        for (int i = 0; i < coords.Count; i++)
+        {
+            lRend.SetPosition(i, coords[i]);
+        }
     }
 }
