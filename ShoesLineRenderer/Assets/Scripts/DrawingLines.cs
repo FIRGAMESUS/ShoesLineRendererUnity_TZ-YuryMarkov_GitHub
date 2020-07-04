cf4d94774a11e8e2ba8307f3e897384c2e5b924d
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-04 16:17:13 +0300

    Start drawing a cross section

--- ShoesLineRenderer/Assets/Scripts/DrawingLines.cs
+++ ShoesLineRenderer/Assets/Scripts/DrawingLines.cs
@@ -10,15 +10,16 @@ public class DrawingLines : MonoBehaviour
     private GameObject lineGeneratorPrefab;
     public List<Vector3> coords;
     public List<Vector3> corners;
+    public float dist;
 
     void Start()
     {
-        model = GameObject.Find("Result");
+        dist = 0.5f;
+        model = GameObject.Find("result");
         lineGeneratorPrefab = GameObject.Find("LineHolder");
         EnumerationCoordsPolygon();
-        CornersToList();
-        WriteMeshPoints(coords);
         WriteMeshPoints(corners);
+
     }
 
     
@@ -29,33 +30,34 @@ public class DrawingLines : MonoBehaviour
 
     void EnumerationCoordsPolygon()
     {
+        CornersToList();
         Mesh mesh = model.transform.GetChild(0).GetComponent<MeshFilter>().mesh;
         Vector3[] verts = mesh.vertices;
         int[] triangles = mesh.triangles;
-        for (int i = 0; i < triangles.Length; i += 3)
+        int step = 1;
+        for (int i = 0; i < triangles.Length; i += step)
         {
-            int i1 = (i + 0);
-            int i2 = (i + 1);
-            int i3 = (i + 2);
-
-            Vector3 v1 = verts[triangles[i1]];
-            Vector3 v2 = verts[triangles[i2]];
-            Vector3 v3 = verts[triangles[i3]];
-            //Debug.Log(v1 + " " + v2 + " " + v3);
-            coords.Add(v1);
-            //coords.Add(v2);
-            //coords.Add(v3);
+            Vector3 v = verts[triangles[i]];
+            if (CheckDist(corners, v, dist))
+            {
+                coords.Add(v);
+                Debug.Log(v);
+            }
         }
 
+        WriteMeshPoints(coords);
     }
 
     void WriteMeshPoints(List<Vector3> points)
     {
         GameObject newLineGen = Instantiate(lineGeneratorPrefab);
+        //newLineGen.transform.SetPositionAndRotation(model.transform.position, model.transform.rotation);
+        //newLineGen.transform.localScale = model.transform.localScale;
         LineRenderer lRend = newLineGen.GetComponent<LineRenderer>();
         lRend.positionCount = points.Count;
         for (int i = 0; i < points.Count; i++)
         {
+            points[i] += model.transform.localPosition;
             lRend.SetPosition(i, points[i]);
         }
     }
@@ -70,10 +72,28 @@ public class DrawingLines : MonoBehaviour
         topright -7.721422806121932 8.215609420586972 0.7073115381738011
         */
         //corners.Add(new Vector3((float)0, (float)0, (float)0));
-        corners.Add(new Vector3((float)7.06552431138356, (float)3.152303227479417, (float)0.5798208855947808));
-        corners.Add(new Vector3((float)3.520846622706104, (float)-7.299853716696669, (float)0.852738011666962));
-        corners.Add(new Vector3((float)-11.26581889156483, (float)-2.161200109859713, (float)1.420210236748185));
-        corners.Add(new Vector3((float)-7.721422806121932, (float)8.215609420586972, (float)0.7073115381738011));
+        corners.Add(new Vector3(7.06552431138356f, 3.152303227479417f, 0.5798208855947808f));
+        corners.Add(new Vector3(3.520846622706104f, -7.299853716696669f, 0.852738011666962f));
+        corners.Add(new Vector3(-11.26581889156483f, -2.161200109859713f, 1.420210236748185f));
+        corners.Add(new Vector3(-7.721422806121932f, 8.215609420586972f, 0.7073115381738011f));
 
+    }
+
+    bool CheckDist(List<Vector3> pointsPlane, Vector3 p, float dist)
+    {
+        Vector3 t1 = pointsPlane[0];
+        Vector3 t2 = pointsPlane[1];
+        Vector3 t3 = pointsPlane[2];
+        float a = t1.y * (t2.z - t3.z) + t2.y * (t3.z - t1.z) + t3.y * (t1.z - t2.z);
+        float b = t1.z * (t2.x - t3.x) + t2.z * (t3.x - t1.x) + t3.z * (t1.x - t2.x);
+        float c = t1.x * (t2.y - t3.y) + t2.x * (t3.y - t1.y) + t2.x * (t1.y - t2.y);
+        float d = -1 * (t1.x * (t2.y * t3.z - t3.y * t2.z) + t2.x * (t3.y * t1.z - t1.y * t3.z) + t3.x * (t1.y * t2.z - t2.y * t1.z));
+
+        float numerator = a * p.x + b * p.y + c * p.z + d;
+        float denominator = (float)System.Math.Sqrt(a*a + b*b + c*c);
+        float r = (numerator / denominator);
+
+        if (System.Math.Abs(r - dist) <= dist * 0.5f) return true;
+        else return false;
     }
 }
